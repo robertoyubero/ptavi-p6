71e4948fb454d7435debc3a0a2df856f40a2f003
robertoyubero <robertoyubero@gmail.com>
2015-11-27 16:46:45 +0100

    pasado pep8

--- server.py
+++ server.py
@@ -3,7 +3,6 @@
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
 import socketserver
 import sys
 import os
@@ -12,12 +11,11 @@ import os
 try:
 
     IP_DESTINO = sys.argv[1]
-    PUERTO_DESTINO = sys.argv[2]
+    PUERTO_DEST = sys.argv[2]
     FICHERO = sys.argv[3]
 
 except IndexError:
     print("Usage: python server.py IP port audio_file")
-
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -26,31 +24,30 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     metodos_sip = ["INVITE", "ACK", "BYE"]
 
-
     def envio_RTP(self):
         """
         Gestion del envio de paquetes RTP: envio el paquete al cliente
         """
         # envio RTP
-        paquete_RTP = ('./mp32rtp -i ' + IP_DESTINO + ' -p ' + PUERTO_DESTINO + ' < '
-        + FICHERO)
-        print("Enviando paquete RTP...")
+        paquete_RTP = ('./mp32rtp -i ' + IP_DESTINO + ' -p 23032 < '
+                       + FICHERO)
+        print("Enviando paquete RTP...\n")
         os.system(paquete_RTP)
-        return ("Finalizado envio de paquetes RTP...")
-
+        return (FICHERO)
 
     def setcode_200(self):
         """
         Enviamos peticion aceptada
         """
-        respuesta = 'SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\nSIP/2.0 200 OK\r\n\r\n'
+        respuesta = ('SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\n' +
+                     'SIP/2.0 200 OK\r\n\r\n')
         return respuesta
 
     def setcode_400(self):
         """
         La peticion NO fue aceptada por estar mal formada
         """
-        respuesta = 'SIP/2.0 400 Bad Request\r\n\r\n'
+        respuesta = ('SIP/2.0 400 Bad Request\r\n\r\n')
         return respuesta
 
     def setcode_405(self):
@@ -59,7 +56,6 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         """
         respuesta = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
         return respuesta
-
 
     def handle(self):
         while 1:
@@ -77,20 +73,23 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             dir_SIP_cliente = recorte.split(" SIP/2.0")[0]
             IP_cliente = dir_SIP_cliente.split('@')[-1]
 
-            print("Recibido: " +  line.split('\r')[0])
+            print("Recibido: " + line.split('\r')[0] + '\n')
             respuesta = b''
             if peticion_cliente == 'INVITE':
                 respuesta = self.setcode_200()
                 self.wfile.write(bytes(respuesta, 'utf-8'))
 
             elif peticion_cliente == 'BYE':
-                #self.tratar_bye()
-                print('DEBO GESTIONAR: BYE')
-                respuesta = 'DEBO GESTIONAR: BYE'
+                #le devuelvo 200OK
+                respuesta = 'SIP/2.0 200 OK\r\n\r\n'
+                self.wfile.write(bytes(respuesta, 'utf-8'))
+                print(dir_SIP_cliente + " ha cerrado la conexion\n")
 
             elif peticion_cliente == 'ACK':
                 #envio el paquete RTP, e imprimo que lo he enviado
                 respuesta = self.envio_RTP()
+                self.wfile.write(bytes(respuesta, 'utf-8'))
+
             else:
                 #metodo no admitido
                 respuesta = self.setcode_405()
@@ -100,10 +99,6 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 if __name__ == "__main__":
 
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer((IP_DESTINO, int(PUERTO_DESTINO)), EchoHandler)
+    serv = socketserver.UDPServer((IP_DESTINO, int(PUERTO_DEST)), EchoHandler)
     print("Listening...\n")
     serv.serve_forever()
-
-
-    #except:
-        #print("Usage: python server.py IP port audio_file")
